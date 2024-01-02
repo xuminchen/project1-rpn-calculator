0865f76d055b7947f3f05d59750b2ba396622e59
xuminchen <simon_1101@163.com>
2024-01-02 21:02:14 +0800

    complete Stack class.

--- Stack.py
+++ Stack.py
@@ -1,4 +1,4 @@
-#class Stack - the main class for Stack seen in lecture 5. 
+# class Stack - the main class for Stack seen in lecture 5.
 # Reminder: stack is a private variable
 # To complete: *  __str__ method must be included that displays the stack in reverse
 # order from the bottom to the top (with index 0 at the bottom)
@@ -7,37 +7,59 @@
 #             * flush method: to empty the stack
 
 class Stack:
-    #constructor
+    # constructor
     def __init__(self):
         self.__stack = []  # create private stack
-    #methods
-    def pop(self):  #pop the item
+
+    # methods
+
+    def __str__(self):
+        s = ""
+        for i in range(self.getSize()):
+            s += f"{i}\t{self.__stack[i]}\n"
+        return s
+
+    def swap(self):
+        a = self.__stack.pop()
+        b = self.__stack.pop()
+        self.push(a)
+        self.push(b)
+        return self.__stack
+
+    def copy(self):
+        if self.isEmpty(): return None
+        return self.push(self.__stack[-1])
+
+    def flush(self):
+        for i in range(self.getSize()):
+            self.pop()
+        return None
+
+    def pop(self):  # pop the item
         if self.isEmpty(): return None
         return self.__stack.pop()
 
-    def peek(self): #peek the item (no removal)
+    def peek(self):  # peek the item (no removal)
         if self.isEmpty(): return None
-        return self.__stack[len(self.__stack)-1]
+        return self.__stack[len(self.__stack) - 1]
 
-    def push(self, item): #push the item
+    def push(self, item):  # push the item
         self.__stack.append(item)
 
-    def getSize(self):           #return stack size
+    def getSize(self):  # return stack size
         return len(self.__stack)
 
-    def isEmpty(self): #check if stack empty
-        return self.getSize()==0
-
-
+    def isEmpty(self):  # check if stack empty
+        return self.getSize() == 0
 
 
 ################################
 ################################
 
 def main():
-    mystack=Stack()
-    for i in range(1,4): mystack.push(i*10)
-    
+    mystack = Stack()
+    for i in range(1, 4): mystack.push(i * 10)
+
     print("test __str__")
     print(mystack)
 
@@ -53,8 +75,8 @@ def main():
     mystack.flush()
     mystack.push(11)
     print(mystack)
-    
+
 
 ## call the main function if this file is directly executed
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
